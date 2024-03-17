0f832911a0bd56790b03b62dbf44fc6f450e9dbb
Ronald Huereca <ronald@mediaron.com>
2024-03-17 02:08:34 -0500

    Doing prod build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '67bb31945436e51fa097');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5ac4071c0e521772b5e8');
